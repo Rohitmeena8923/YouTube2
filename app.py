4b6a876ca6ede2ab0a73ee45259bcf399836254c
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-05-15 15:59:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import logging
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
